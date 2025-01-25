f4580b659196f3e3f138541722be5ca9574600b3
Paper <ericliu88888@hotmail.com>
2025-01-25 13:26:16 +0800

    initialize the home page

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DoContrib",
+  description: "追蹤並管理團隊成員貢獻進度的專業平台，幫助您更有效地監控專案發展。",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="zh-TW">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <div className="bg-black w-full h-full flex items-center justify-center">
+            <h1 className="text-8xl font-bold font-playwrite text-white">DoContrib</h1>
+        </div>
+    </div>
+  );
+}
