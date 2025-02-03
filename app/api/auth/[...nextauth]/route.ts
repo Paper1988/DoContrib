f2ebae86ccffd847bf4fea9e2a61b3a6708209e8
Paper <ericliu88888@hotmail.com>
2025-02-03 13:33:33 +0800

    [...nextauth]/route.ts exportation fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
