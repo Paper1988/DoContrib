836ae8993939abbd913a6ea495fa2bcc4c021aba
Paper <ericliu88888@hotmail.com>
2025-04-06 20:07:19 +0800

    調整 GET 函數的參數名稱，將 req 改為 _req，不然會有Error: 'req' is defined but never used. 的問題

--- app/api/documents/route.ts
+++ app/api/documents/route.ts
@@ -1,7 +1,7 @@
 import { getSupabaseServerClient } from '@/lib/supabaseClient'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: NextRequest) {
+export async function GET(_req: NextRequest) {
     const supabase = await getSupabaseServerClient()
 
     const {
